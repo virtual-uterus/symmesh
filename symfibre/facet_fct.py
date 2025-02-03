7120cd22d65a40cdceb6b7052b98d9390e7fa14b
Mathias <mroe734@aucklanduni.ac.nz>
2025-02-04 12:08:24 +1300

    Updated the facet functions

--- symfibre/facet_fct.py
+++ symfibre/facet_fct.py
@@ -9,6 +9,7 @@ Date: 01/25
 """
 
 import dolfin as df
+import numpy as np
 
 
 class InnerLayer(df.SubDomain):
@@ -24,10 +25,15 @@ class InnerLayer(df.SubDomain):
         on_boundary -- bool, true if conditions are satisfied
 
         """
-        return coord[2] <= 0.5 and coord[2] >= 0.0 and on_boundary
+        return on_boundary
+        # return coord[1] <= 0.039 and coord[1] >= -0.039 and on_boundary
 
 
 class OuterLayer(df.SubDomain):
+    def __init__(self, outer_coords):
+        super().__init__()
+        self.outer_coords = outer_coords
+
     def inside(self, coord, on_boundary):
         """Identifies outer layer elements
 
@@ -40,7 +46,10 @@ class OuterLayer(df.SubDomain):
         on_boundary -- bool, true if conditions are satisfied
 
         """
-        return coord[2] >= 0.5 and on_boundary
+        if on_boundary:
+            norms = np.linalg.norm(coord - self.outer_coords, axis=1)
+            return min(norms) < 0.025
+        return False
 
 
 class BaseLayer(df.SubDomain):
@@ -56,4 +65,6 @@ class BaseLayer(df.SubDomain):
         on_boundary -- bool, true if conditions are satisfied
 
         """
-        return coord[2] <= 0.0 and on_boundary
+        if coord[2] >= 0.98 and on_boundary:
+            return True
+        return False
