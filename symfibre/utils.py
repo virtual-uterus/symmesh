8ecb2e2b71e986c5e843d30c67281613a2a5579b
Mathias <mroe734@aucklanduni.ac.nz>
2025-02-05 09:32:13 +1300

    Wrote a function to calculate element centre coordinates

--- symfibre/utils.py
+++ symfibre/utils.py
@@ -9,6 +9,7 @@ Date: 01/25
 """
 
 import numpy as np
+import dolfin as df
 
 
 def write_ortho_file(file_path, fibres, sheets, normals):
@@ -50,7 +51,7 @@ def extract_coordinates(points, fibre_fct, sheet_fct, normal_fct):
     """Extracts the coordinates of the ldrb FiberSheetSystem at specific points
 
     Arguments:
-    points -- np.array, list of point coordinates to evaluate
+    points -- np.array(N, 3), list of point coordinates to evaluate
     the FiberSheetSystem functions.
     fibre_fct -- df.Function, FiberSheetSystem fibre function.
     sheet_fct -- df.Function, FiberSheetSystem sheet function.
@@ -75,3 +76,30 @@ def extract_coordinates(points, fibre_fct, sheet_fct, normal_fct):
         normals.append(normal_fct(point))
 
     return np.array(fibres), np.array(sheets), np.array(normals)
+
+
+def element_centres(mesh):
+    """Extracts element centres from a given mesh
+
+    Arguments:
+    mesh -- df.Mesh, mesh to extract the centres from.
+
+    Return:
+    centres -- np.array(N, 3), element centres.
+
+    Raises:
+    TypeError -- if the mesh is not a df.Mesh
+
+    """
+    try:
+        assert type(mesh) is type(df.Mesh())
+
+    except AssertionError:
+        raise TypeError("mesh should be a df.Mesh")
+
+    centres = np.zeros((mesh.num_cells(), 3))  # Initialise empty array
+
+    for i, ele in enumerate(mesh.cells()):
+        centres[i] = np.mean(mesh.coordinates()[ele], axis=0)
+
+    return centres
