71f4b78d7b9080a436046e14832a94f01fc4b40f
Mathias <mroe734@aucklanduni.ac.nz>
2025-03-20 10:31:55 +1300

    Updated the init function

---
+++ symmesh/__init__.py
@@ -0,0 +1,17 @@
+"""
+symmesh
+========
+A Python package with tools for manipulating
+uterine meshes.
+
+Modules:
+- constants: Constant values used in the package.
+- utils: General utilities for handling data and file operations.
+- facet_fct: facet functions for using with ldrb.
+- mesh: function for extracting mesh information.
+- plot: plotting functions.
+- paraview_fct: functions that use the paraview API.
+
+"""
+
+__version__ = "0.2.0"
